7f9aa17fec244ac52c4c49589c1e8503995e8f75
j.developer.days@gmail.com <j.developer.days@gmail.com>
2022-08-03 16:49:40 +0300

    lesson 3. java 11 feature run single class without compilation

---
+++ src/Single.java
@@ -0,0 +1,15 @@
+import com.Book;
+
+public class Single {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        new SecondClass();
+        new Book();
+    }
+
+}
+
+class SecondClass{
+
+}
