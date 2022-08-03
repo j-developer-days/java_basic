169a663478dd330ba638322525bec1690e065a7a
j.developer.days@gmail.com <j.developer.days@gmail.com>
2022-08-03 15:06:51 +0300

    lesson 2. java se. java main method and his parameters

---
+++ src/Lion.java
@@ -0,0 +1,9 @@
+public class Lion {
+
+    public static void main(String ... args){
+        //Person.main(new String[]{"First", "2", "100"});
+        //Person.main(null);
+        Person.main(new String[]{});
+    }
+
+}
--- src/Person.java
+++ src/Person.java
@@ -22,10 +22,26 @@ public class Person {
      * java doc comment
      * @author j.developer.days
      * */
-
+/*
     public static void main(String [] args){
         System.out.println("Hello from person class");
-        Person.main(null);
+        System.out.println("This is first param - " + args[0]);
+        System.out.println("Second param - " + args[1]);
+        System.out.println("param #3 is - " + args[2]);
+    }
+  */
+    /*public static void main(String args []){
+        System.out.println("Hello from person class");
+        System.out.println("This is first param - " + args[0]);
+        System.out.println("Second param - " + args[1]);
+        System.out.println("param #3 is - " + args[2]);
+    }*/
+
+    public static void main(String ... options){
+        System.out.println("Hello from person class");
+        System.out.println("This is first param - " + options[0]);
+        System.out.println("Second param - " + options[1]);
+        System.out.println("param #3 is - " + options[2]);
     }
 
 }
